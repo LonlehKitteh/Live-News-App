6172aabbde6acc982f8bf8e7a7388e7cf0c06f40
LonlehKitteh <krzysztof02gach@gmail.com>
2023-03-07 20:25:42 +0100

    setting up the app

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/About.module.scss";
+
+const about = () => {
+    return <div className={styles.testing}>about</div>;
+};
+
+export default about;
