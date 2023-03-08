7ae30a8ddbbc22f3b61e3d7731f2e0664eaa2e99
LonlehKitteh <krzysztof02gach@gmail.com>
2023-03-08 22:15:47 +0100

    fatching data using stepzen

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Documentation
 
 1. Convert to next.js 13 project by adding experimental feature (server components)
+
+## Reminder to commit
+
+1. yarn format
+2. git add .
+3. git commit -m 'commit'
+4. testing check
+5. git push
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
 
-function Homepage() {
-    return <div className="text-3xl font-bold underline">Homepage</div>;
+async function Homepage() {
+    // fetch the news data
+    const news: NewsResponse = await fetchNews(categories.join(","));
+
+    console.log(news);
+
+    return <div className="text-3xl font-bold underline"></div>;
 }
 
 export default Homepage;
